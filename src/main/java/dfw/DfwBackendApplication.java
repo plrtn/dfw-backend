1879556cb3e55f8c8f4b27611c9e2319e41f5782
Phil <plrtn@protonmail.com>
2022-12-14 11:23:23 -0500

    initial commit

---
+++ src/main/java/dfw/DfwBackendApplication.java
@@ -0,0 +1,13 @@
+package dfw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfwBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfwBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dfw/DfwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dfw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfwBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
