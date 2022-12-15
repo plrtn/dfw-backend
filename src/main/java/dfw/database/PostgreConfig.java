67e8bc92ab586b94e811013fa9e7366b06e27535
Phil <plrtn@protonmail.com>
2022-12-15 12:43:57 -0500

    add spring postgre dep, add db connector and test, add env.properties file to .gitignore

---
+++ src/main/java/dfw/database/PostgreConfig.java
@@ -0,0 +1,30 @@
+package dfw.database;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:env.properties")
+public class PostgreConfig {
+	
+	@Bean
+	@ConfigurationProperties("app.database")
+	DataSource getDataSource() {
+		return DataSourceBuilder
+				.create()
+				.driverClassName("org.postgresql.Driver")
+				.build();
+	}
+	
+	@Bean
+	JdbcTemplate getJdbcTemplate() {
+		return new JdbcTemplate(this.getDataSource());
+	}
+}
--- src/test/java/dfw/DfwBackendApplicationTests.java
+++ src/test/java/dfw/DfwBackendApplicationTests.java
@@ -1,13 +1,52 @@
 package dfw;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.SQLException;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 @SpringBootTest
 class DfwBackendApplicationTests {
+	
+	@Autowired
+	JdbcTemplate jdbc;
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
-
+	
+	@Test
+	void hasDBConnection() throws SQLException {
+		assertThat(jdbc).isNotNull();
+		assertThat(jdbc.getDataSource().getConnection()).isNotNull();
+	}
+	
+	@Test
+	void canQueryDB() {
+		String sql = """
+				select 1 as foo
+				""";
+		
+		assertThat(jdbc.queryForMap(sql)).isNotNull().containsKey("foo");
+	}
+	
+	@Test
+	void canQueryTable() {
+		String sql = """
+					select * from dfw_demo
+					limit 1
+				""";
+		
+		SqlRowSet result = jdbc.queryForRowSet(sql);
+		result.first();
+		assertThat(result.getMetaData().getColumnNames()).contains("Key", "income", "population", "spatialobj");
+	}
 }
