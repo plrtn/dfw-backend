fc2557665806556c90232d2d747d546ccd1c05ea
Phil <plrtn@protonmail.com>
2022-12-20 12:29:52 -0500

    add function to compute totalPop

--- src/main/java/dfw/core/service/InterpolationService.java
+++ src/main/java/dfw/core/service/InterpolationService.java
@@ -2,19 +2,13 @@ package dfw.core.service;
 
 import java.util.List;
 
-import dfw.core.Coordinate;
-import dfw.core.InterpolationStrategy;
-import dfw.core.Tract;
+import dfw.core.model.Tract;
 import dfw.util.MathUtil;
 
 public final class InterpolationService {
-	
- public static List<Tract> computeTracts(Coordinate c, double radius, InterpolationStrategy is) {
-	 return null;
- }
  
- public static double computeAveragePopulation (List<Tract> tracts) {
-	 return MathUtil.average(tracts.stream().map(x -> x.population() * x.ratio()));
+ public static double computeTotalPopulation (List<Tract> tracts) {
+	 return MathUtil.sum(tracts.stream().map(x -> x.population() * x.ratio()));
  }
  
  public static double computeMedianIncome(List<Tract> tracts) {
